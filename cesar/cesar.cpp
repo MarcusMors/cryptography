0594a219c19e1af44f3637aa7964a46656d4dc51
Marcus Mors <alivezeh@gmail.com>
2021-04-22 16:49:25 -0500

    :sparkles: cripto analysis
    also, fix a bug

--- cesar/cesar.cpp
+++ cesar/cesar.cpp
@@ -1,76 +1,168 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-void cesar(string &alphabet, int key, string &msg)
+int min(int a, int b)
 {
-	int alphabetSize = alphabet.size();
-	string ciphered = "";
-	for (int i = 0; i < msg.size(); i++)
-		for (int j = 0; j < alphabetSize; j++)
-			if (msg[i] == alphabet[j])
-				ciphered += alphabet[abs(j + key) % alphabetSize];
-
-	msg = ciphered;
+	return a < b ? a : b;
 }
+
+unsigned levenshteinDistance(string strA, string strB)
+{
+	int sizeA = strA.size();
+	int sizeB = strB.size();
+	unsigned shortest = min(sizeA, sizeB);
+	unsigned distance = abs(sizeA - sizeB);
+	for (int i = 0; i < shortest; i++)
+	{
+		if (strA[i] != strB[i])
+		{
+			distance++;
+		}
+	}
+	return distance;
+}
+
+class Cesar
+{
+private:
+	string alphabet = "abcdefghijklmnopqrstuvwxyz";
+	// string alphabet = "abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ";
+	// string alphabet = " abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ";
+	// string alphabet = "1 abcdefghijk2lmnopqrstu3vwxyz45ABCDFGH6IJKLMN7OP8QRSTUVW9XYZ";
+
+public:
+	void cipher(string &msg, int iKey)
+	{
+		int alphabetSize = alphabet.size();
+		cout << "alphabetSize : " << alphabetSize << endl;
+		int size = msg.size();
+		int key = (iKey % alphabetSize);
+		string ciphered = "";
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < alphabetSize; j++)
+				if (msg[i] == alphabet[j])
+				{
+					ciphered += alphabet[(j + key) % alphabetSize];
+					break;
+				}
+		msg = ciphered;
+	}
+	void descipher(string &msg, int iKey)
+	{
+		int alphabetSize = alphabet.size();
+		int size = msg.size();
+		cout << "alphabetSize : " << alphabetSize << endl;
+		int key = (iKey % alphabetSize);
+		string ciphered = "";
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < alphabetSize; j++)
+				if (msg[i] == alphabet[j])
+				{
+					ciphered += alphabet[(alphabetSize + j - key) % alphabetSize];
+					break;
+				}
+		msg = ciphered;
+	}
+	void cryptoAnalysis(string &msg) // desciph a message
+	{
+		int size = msg.size();
+		int alphabetSize = alphabet.size();
+		for (int key = 0; key < alphabetSize; key++)
+		{
+			string ciphered = "";
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < alphabetSize; j++)
+					if (msg[i] == alphabet[j])
+					{
+						ciphered += alphabet[(alphabetSize + j - key) % alphabetSize];
+						break;
+					}
+			cout << "key : " << key << endl;
+			cout << ciphered << endl;
+		}
+		//* the other way
+		// string directory = "dictionaries/";
+		// int nFiles = 2;
+		// string files[nFiles] = {
+		// 	"english.txt",
+		// 	"spanish.txt"};
+		// for (int i = 0; i < nFiles; i++)
+		// {
+		// 	string path = directory + files[i];
+		// 	ifstream dictionary;
+		// 	dictionary.open(path, ios::in);
+		// 	if (!dictionary)
+		// 	{
+		// 		cout << "Dictionary not found" << endl;
+		// 		cout << "Do manual check" << endl;
+		// 	}
+		// 	else
+		// 	{
+		// 		string word;
+		// 		while (true)
+		// 		{
+		// 			dictionary >> word;
+		// 			if (dictionary.eof())
+		// 				break;
+		// 			cout << word << endl;
+		// 		}
+		// 		dictionary.close();
+		// 	}
+		// }
+	}
+};
 
 class Receiver
 {
 private:
 	int key;
-	string alphabet;
 
 public:
-	Receiver(int iKey, string iAlphabet)
+	Receiver(int iKey)
 	{
 		key = iKey;
-		alphabet = iAlphabet;
 	}
 	void descipher(string &msg)
 	{
-		cesar(alphabet, key * -1, msg);
+		Cesar().descipher(msg, key);
 	}
 };
 class Transmitter
 {
 private:
 	int key;
-	string alphabet;
 
 public:
-	Transmitter(int iKey, string iAlphabet)
+	Transmitter(int iKey)
 	{
-		alphabet = iAlphabet;
 		key = iKey;
 	}
 	void cipher(string &msg)
 	{
-		cesar(alphabet, key, msg);
+		Cesar().cipher(msg, key);
 	}
 };
 
 int main()
 {
-	// string alphabet = "abcdefghijklmnopqrstuvwxyz";
-	// string alphabet = "abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ";
-	// string alphabet = " abcdefghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ";
-	string alphabet = "1 abcdefghijk2lmnopqrstu3vwxyz45ABCDFGH6IJKLMN7OP8QRSTUVW9XYZ";
 	string msg;
-	cout << "input message: ";
+	cout << "input message\t: ";
 	getline(cin, msg);
 	int key;
-	cout << "input key    : ";
+	cout << "input key\t: ";
 	cin >> key;
-	Receiver receiver(key, alphabet);
-	Transmitter transmitter(key, alphabet);
+	Receiver receiver(key);
+	Transmitter transmitter(key);
 	// string cipheredMsg = transmitter.cipher(msg);
 	transmitter.cipher(msg);
-	cout << "ciphered message" << endl;
-	cout << msg << endl;
-	// cout << "crypto analysis" << endl;
-	// cout << cryptoAnalysis(msg) << endl;
+	cout << "ciphered\t: " << msg << endl;
+	cout << "crypto analysis" << endl;
+	string interception = msg;
+	Cesar().cryptoAnalysis(interception);
 	receiver.descipher(msg);
-	cout << "desciphered message" << endl;
-	cout << msg << endl;
+	cout << "desciphered\t: " << msg << endl;
+
 	return 0;
 }
